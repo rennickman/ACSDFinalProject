689b71a8c4db1ef96cb59917f191c5aa04d999f9
Rennlad <rennickman@gmail.com>
2022-04-04 12:31:06 +0100

    added apikey and token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+import React, { useState } from 'react';
+import { footballApi } from './apiKeys';
+import axios from 'axios';
+
 
 function App() {
 
 
+    const [todaysMatches, setTodaysMatches] = useState([]);
+    const [leagueTable, setLeagueTable] = useState([]);
+    const [cometitionMatches, setCompetitionMatches] = useState([]);
+    
+   
+
+    // Method for fetching today's matches for subscribed competitions
+    const fetchTodaysMatches = async () => {
+        const data = await axios.get(footballApi.link + "matches", 
+            { headers: { "X-Auth-Token": footballApi.token } });
+        setTodaysMatches(data.data.matches);
+    };
+
+
+    // Method for fetching league table - must enter league ID eg. Prem = PL
+    const fetchLeagueTable = async () => {
+        const data = await axios.get(footballApi.link + "competitions/PL/standings", 
+            { headers: { "X-Auth-Token": footballApi.token } });
+        setLeagueTable(data.data.standings[0].table);
+    };
+
+
+    // Method for fetching all matches for particular competition - filter by matchday
+    const fetchCompetitionMatches = async () => {
+        const data = await axios.get(footballApi.link + "competitions/PL/matches?matchday=15",
+            { headers: { "X-Auth-Token": footballApi.token } });
+        setCompetitionMatches(data.data.matches);
+    };
+
+    
+    
+
     return (
         <div className="app">
-          
-          
+            {/* Buttons for testing API calls */}
+            <button onClick={fetchTodaysMatches}>Test Todays Matches</button>
+            <button onClick={fetchLeagueTable}>Test League Table</button>
+            <button onClick={fetchCompetitionMatches}>Test Competition Matches</button>
         </div>
     );
-}
+};
 
 export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// API info for the Football Data API -  use a HTTP header named "X-Auth-Token" with the token as the value.
+export const footballApi = {
+    link: "https://api.football-data.org/v2/",
+    token: "db0116e51f3b4caa98292319ba53525c"
+};
