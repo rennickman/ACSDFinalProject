55d44a16151f7f59bd8d2747ece0f39786afa19a
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-04-22 19:16:01 +0100

    styling added to game weeks

--- src/components/Game Week Comps/CurrentGameWeek.js
+++ src/components/Game Week Comps/CurrentGameWeek.js
@@ -1,16 +1,67 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+import { format } from 'date-fns'
+import { mapOddsLeagues } from "../../helperFunctions";
+import { Container, Card, Row, Col } from "react-bootstrap";
+import './currentGameWeek.css'
 
 const CurrentGameWeek = (props) => {
-  const { home, away, time } = props;
+  const { home, away, time, leagueName } = props;
+  const [ leagueTitle, setLeagueTitle ] = useState("")  
+
+  //time formatting
+  const gameDate = format(new Date(`${time}`), 'dd-MM-yyyy');
+  const gameTime = format(new Date(`${time}`), 'k.mm');
+
+  //League Titles
+  const settingLeagueTitle = () => {
+    if (leagueName === 733) {
+      setLeagueTitle(mapOddsLeagues[0].name)
+    }
+    else if (leagueName === 735){
+     setLeagueTitle(mapOddsLeagues[5].name)
+    }
+    else if (leagueName === 757){
+     setLeagueTitle(mapOddsLeagues[3].name)
+    }
+    else if (leagueName === 746){
+     setLeagueTitle(mapOddsLeagues[2].name)
+    }
+    else if (leagueName === 742){
+     setLeagueTitle(mapOddsLeagues[1].name)
+    }
+    else if (leagueName === 380){
+     setLeagueTitle(mapOddsLeagues[4].name)
+    }
+  }
+    useEffect(() => {
+      settingLeagueTitle();
+    }, [])
   return(
-    <Container>
-      <div>
-        <span>
-          {time} - {home} vs {away}
-        </span>
-      </div>
+    <Container >
+      <Card >
+        <Card.Header>
+          <Row className="game_week_headers">
+            <Col sm={6}> {leagueTitle} </Col>
+            <Col sm={6} id='game_date_header'> {gameDate} </Col>
+          </Row>
+        </Card.Header >
+        <Card.Body id="game_week_container">
+          <Row>
+            <Col sm={5} className='teams'>
+              <h5>{home}</h5>
+            </Col>
+            <Col sm={2} className='teams'>
+              <Card.Text> {gameTime} </Card.Text>
+            </Col>
+            <Col sm={5} className='teams'>
+              <h5>{away}</h5>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
     </Container>
-  )
+  );
 }
 export default CurrentGameWeek;
--- src/pages/GameWeek/GameWeekMatches.js
+++ src/pages/GameWeek/GameWeekMatches.js
@@ -1,20 +1,17 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
-import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { footballApi3, config, config1, config2 } from '../../apiKeys'
+import CurrentGameWeek from '../../components/Game Week Comps/CurrentGameWeek'
+import './gameWeekMatches.css'
 
-import { mapAPIs } from '../../apiKeys';
-import { footballApi3, config, config1, config2 } from '../../apiKeys';
-import Sidebar from '../../components/Sidebar/Sidebar';
-import CurrentGameWeek from '../../components/Game Week Comps/CurrentGameWeek';
-
-function GameWeekMatches({ username, favouriteTeam }) {
-
+function GameWeekMatches() {
 
   //Game Week Counters
-  const [matchDayCounter, setMatchDayCounter] = useState("")
-  const [matchDayCounterGermany, setMatchDayCounterGermany] = useState("")
-  const [matchDayCounterChampionship, setMatchDayCounterChampionship] = useState("")
+  const [matchDayCounter, setMatchDayCounter] = useState("1")
+  const [matchDayCounterGermany, setMatchDayCounterGermany] = useState("1")
+  const [matchDayCounterChampionship, setMatchDayCounterChampionship] = useState("1")
 
   //Leagues states
   const [plMatches, setPlMatches] = useState([]);
@@ -24,83 +21,42 @@ function GameWeekMatches({ username, favouriteTeam }) {
   const [elcMatches, setElcMatches] = useState([]);
   const [pdMatches, setPdMatches] = useState([]);
 
-  // Value being searched for in search bar
-  const [loading, setLoading] = useState(true);
-
-  //Length of the mapAPIs
-  const apiLength = Object.keys(mapAPIs).length
-
-  // React-router-dom Method for pushing to different page
-  const history = useNavigate()
-  
-  //Error for Home page
-  const [error, setError] = useState("");
-
   const fetchCurrentGameWeek = async () => {
-            //Makes API calls to different token keys untill one is successful
-            var apiCall = false;
-            var i = 0;
-            do{
-                try{
-            //fetching data
-            const getPL = await axios.get("http://api.football-data.org/v2/competitions/" + "PL" + "/matches?matchday=" + `${matchDayCounter}`, {headers:{"X-Auth-Token": mapAPIs[i].token}} )
-            const getSA = await axios.get("http://api.football-data.org/v2/competitions/" + "SA" + "/matches?matchday=" +`${matchDayCounter}`, {headers:{"X-Auth-Token": mapAPIs[i].token}})
-            const getFL1 = await axios.get("http://api.football-data.org/v2/competitions/" + "FL1" + "/matches?matchday=" +`${matchDayCounter}`, {headers:{"X-Auth-Token": mapAPIs[i].token}})
-            const getBL1 = await axios.get("http://api.football-data.org/v2/competitions/" + "BL1" + "/matches?matchday=" +`${matchDayCounterGermany}`, {headers:{"X-Auth-Token": mapAPIs[i].token}})
-            const getELC = await axios.get("http://api.football-data.org/v2/competitions/" + "ELC" + "/matches?matchday=" +`${matchDayCounterChampionship}`, {headers:{"X-Auth-Token": mapAPIs[i].token}})
-            const getPD = await axios.get("http://api.football-data.org/v2/competitions/" + "PD" + "/matches?matchday=" +`${matchDayCounter}`, {headers:{"X-Auth-Token": mapAPIs[i].token}})
+    //fetching data
+      const getPL = await axios.get( footballApi3.link1 + "PL" + footballApi3.link2 + `${matchDayCounter}`, config)
+      const getSA = await axios.get(footballApi3.link1 + "SA" + footballApi3.link2 +`${matchDayCounter}`, config)
+      const getFL1 = await axios.get(footballApi3.link1 + "FL1" + footballApi3.link2 +`${matchDayCounter}`, config1)
+      const getBL1 = await axios.get(footballApi3.link1 + "BL1" + footballApi3.link2 +`${matchDayCounterGermany}`, config1)
+      const getELC = await axios.get(footballApi3.link1 + "ELC" + footballApi3.link2 +`${matchDayCounterChampionship}`, config2)
+      const getPD = await axios.get(footballApi3.link1 + "PD" + footballApi3.link2 +`${matchDayCounter}`, config2)
 
-            //If the status of the request is ok it stores matches in useState, stops the loop, and displays the data in the webpage
-            if(getPL.status ===  200 && getSA.status ===  200 && getFL1.status ===  200 && getBL1.status ===  200 && getELC.status ===  200 && getPD.status ===  200){
+      //assigning vars to every league
+      axios.all([getPL, getSA, getFL1, getBL1, getELC, getPD]).then(
+      axios.spread((...allData) => {
+       const plData = allData[0].data.matches
+       const saData = allData[1].data.matches
+       const fl1Data = allData[2].data.matches
+       const bl1Data = allData[3].data.matches
+       const elcData = allData[4].data.matches
+       const pdData = allData[5].data.matches
 
-               //assigning vars to every league
-                axios.all([getPL, getSA, getFL1, getBL1, getELC, getPD]).then(
-                axios.spread((...allData) => {
-                  const plData = allData[0].data.matches
-                  const saData = allData[1].data.matches
-                  const fl1Data = allData[2].data.matches
-                  const bl1Data = allData[3].data.matches
-                  const elcData = allData[4].data.matches
-                  const pdData = allData[5].data.matches
-      
-                  //setting matchday counters
-                  const counterForEngItaEspFra = allData[0].data.matches[0].season.currentMatchday
-                  setMatchDayCounter(counterForEngItaEspFra)
-                  const counterGer = allData[3].data.matches[0].season.currentMatchday
-                  setMatchDayCounterGermany(counterGer)
-                  const counterELC = allData[4].data.matches[0].season.currentMatchday
-                  setMatchDayCounterChampionship(counterELC)
+      //setting league data 
+       setPlMatches(plData)
+       setSaMatches(saData)
+       setFl1Matches(fl1Data)
+       setBl1Matches(bl1Data)
+       setElcMatches(elcData)
+       setPdMatches(pdData)
 
-                  //setting league data 
-                  setPlMatches(plData)
-                  setSaMatches(saData)
-                  setFl1Matches(fl1Data)
-                  setBl1Matches(bl1Data)
-                  setElcMatches(elcData)
-                  setPdMatches(pdData)
-                  })
-                );
-
-                apiCall = false;
-                setLoading(false);
-            }
-        }catch {
-            //If it is the third error it redirects to the home page and send the error "Too many requests"
-            if (i===apiLength-1){
-                setError("Too many requests, try again later")
-                console.log("Too many requests, try again later")
-            }else{
-              //If an error is catched keeps the loop running so it makes another call to another apiKey
-              console.log(`There was an error retrieving the data from the API token ${i}`);
-              apiCall = true;
-              setLoading(true);
-            }
-        }
-
-        i++;
-        //Runs three times because that's the number of keys that we have
-    } while(apiCall && i<apiLength);
-
+       //setting matchday counters
+       const counterForEngItaEspFra = allData[0].data.matches[0].season.currentMatchday
+       setMatchDayCounter(counterForEngItaEspFra)
+       const counterGer = allData[3].data.matches[0].season.currentMatchday
+       setMatchDayCounterGermany(counterGer)
+       const counterELC = allData[4].data.matches[0].season.currentMatchday
+       setMatchDayCounterChampionship(counterELC)
+        })
+      );
   } 
  // Function is ran again once the matchday counter is updated to retrieve data for current game week 
   useEffect(() => {
@@ -109,50 +65,29 @@ function GameWeekMatches({ username, favouriteTeam }) {
 
   //Array for each league
   const AllLeagues = [ plMatches, saMatches, fl1Matches, bl1Matches, elcMatches, pdMatches ] 
-  
 
-  //If an error exists redirects to home page and sends the error to be displayed display
-  if (error) {
-    history('/', {state: error});
-  } else if (loading){
-      //Renders the page
-      return(
-          <>  
-              <div className='league-displayed'>
-                  {username && <Sidebar username={username} favouriteTeam={favouriteTeam} />}
-                  <h1>Loading...</h1>
-              </div>
-          </>
-      );
-  } else {
-      //Renders the data
-      return(
-          <>  
-              <div className='league-displayed'>
-                  {username && <Sidebar username={username} favouriteTeam={favouriteTeam} />}
-                  <Container>
-                    {AllLeagues.map((everyLeague, index) => {
-                    return (
-                      <Row key={index}>  
-                      <h1>Weekly Fixtures</h1> 
-                        <Col> 
-                        {everyLeague.map((currentGW) => 
-                          <Link to={'/match/'} state={currentGW} key={currentGW.id}>
-                            <CurrentGameWeek
-                              home={currentGW.homeTeam.name}
-                              away={currentGW.awayTeam.name}
-                              time={currentGW.utcDate} />  
-                          </Link>                   
-                        )}  
-                      </Col>  
-                      </Row>
-                    ) 
-                    })}
-                  </Container>
-              </div>
-          </>
-      );
-  }
+  return (
+    <div>
+        <Container>
+        {AllLeagues.map((everyLeague, index) => {
+         return (
+           <Container key={index}>  
+           <br/>
+           <h2>Games This Week</h2>
+             {everyLeague.map((currentGW) =>
+              <Link to={'/match/'} state={currentGW} key={currentGW.id} className='match_links'>
+               <CurrentGameWeek key={currentGW.id}
+                 leagueName={currentGW.season.id}
+                 home={currentGW.homeTeam.name}
+                 away={currentGW.awayTeam.name}
+                 time={currentGW.utcDate} />             
+             </Link>)}  
+           </Container>
+         ) 
+       })}
+        </Container>
+    </div>
+  )
 }
 
 export default GameWeekMatches
