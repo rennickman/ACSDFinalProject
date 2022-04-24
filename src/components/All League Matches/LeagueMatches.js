5d3883c8d30828dc5d432b63e372f3d93954a5bb
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-04-24 23:45:39 +0100

    styled results and fixtures

--- src/components/All League Matches/LeagueMatches.js
+++ src/components/All League Matches/LeagueMatches.js
@@ -9,6 +9,7 @@ const LeagueMatches = ({ leagueMatch }) => {
 
 //Formatting Time
 const gameDate = format(new Date(`${leagueMatch.utcDate}`), 'dd-MM-yyyy');
+const gameTime = format(new Date(`${leagueMatch.utcDate}`), 'dd-MM-yyyy');
 
     return (
           <Container >
@@ -16,7 +17,7 @@ const gameDate = format(new Date(`${leagueMatch.utcDate}`), 'dd-MM-yyyy');
                 <Card >
               <Card.Header>
                 <Row className="game_week_r_f">
-                  <Col sm={6}> Week {leagueMatch.matchday} </Col>
+                  <Col sm={6} id='week_date_game'> Week {leagueMatch.matchday} </Col>
                   <Col sm={6} id='game_date_r_f'> {gameDate} </Col>
                 </Row>
               </Card.Header >
@@ -27,7 +28,9 @@ const gameDate = format(new Date(`${leagueMatch.utcDate}`), 'dd-MM-yyyy');
                     <h5>{leagueMatch.homeTeam.name}</h5>
                   </Col>
                   <Col sm={2} className='r_f_data'>
-                    <Card.Text className='r_f_text'> {leagueMatch.score.fullTime.homeTeam} - {leagueMatch.score.fullTime.awayTeam} </Card.Text>
+                    <Card.Text className='r_f_text'> {leagueMatch.score.fullTime.homeTeam} - {leagueMatch.score.fullTime.awayTeam} </Card.Text>      
+                    <Card.Text className='match_text'> Kick Off </Card.Text>
+                    <Card.Text className='match_text'> {gameTime} (GMT+1) </Card.Text>
                   </Col>
                   <Col sm={5} className='r_f_data'>
                   <img className='leage_matches_crests' src={clubCrests.link1 + leagueMatch.awayTeam.id + clubCrests.link2}></img>
